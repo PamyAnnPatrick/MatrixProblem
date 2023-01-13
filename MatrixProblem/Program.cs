f97fc63e941289a0ee0338e2c4c82d62f82a6e3d
Shibin <Shibin@DESKTOP-STSRECM>
2023-01-12 19:05:42 -0500

    Add project files.

---
+++ MatrixProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MatrixProblem;
+
+
+Solution s = new();
+//int[][] a = new int[5][];
+//a[0] = new int[5] { 1, 2, 2, 3, 5 };
+//a[1] = new int[5] { 3, 2, 3, 4, 4 };
+//a[2] = new int[5] { 2, 4, 5, 3, 1 };
+//a[3] = new int[5] { 6, 7, 1, 4, 5 };
+//a[4] = new int[5] { 5, 1, 1, 2, 4 };
+
+int[][] a = new int[3][];
+a[0] = new int[3] { 10, 10, 10 };
+a[1] = new int[3] { 10, 1, 10 };
+a[2] = new int[3] { 10, 10, 10 };
+
+//[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]
+//int[][] a = new int[5][];
+//a[0] = new int[5] { 1, 2, 2, 3, 5 };
+//a[1] = new int[5] { 3, 2, 3, 4, 4 };
+//a[2] = new int[5] { 2, 4, 5, 3, 1 };
+//a[3] = new int[5] { 6, 7, 1, 4, 5 };
+//a[4] = new int[5] { 5, 1, 1, 2, 4 };
+var r=s.PacificAtlantic(a);
+r.ToList().ForEach(x => Console.WriteLine($"{x.Item1},{x.Item2}"));
+
+Console.ReadLine();
---
+++ MatrixProblem/Solution.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixProblem
+{
+    public class Solution
+    {
+        int[,] flows;
+        HashSet<(int, int)> res = new();
+        HashSet<(int, int)> queueHash = new();
+        int rows, cols;
+        public HashSet<(int, int)> PacificAtlantic(int[][] heights)
+        {
+            rows = heights.Length;
+            cols = heights[0].Length;
+            flows = new int[rows, cols];
+
+            Queue<(int r, int c)> queue = new();
+
+            //Pacific Flows  
+            for (int r = 1; r < rows; r++)
+            {
+                queue.Enqueue((r, 0));
+                queueHash.Add((r, 0));
+            }
+
+            for (int c = 0; c < cols; c++)
+            {
+                queue.Enqueue((0, c));
+                queueHash.Add((0, c));
+            }
+            queue = BFS(queue, heights, true);
+
+            //Atlantic Flows  
+            for (int r = 0; r < rows; r++)
+            {
+                queue.Enqueue((r, cols - 1));
+                queueHash.Add((r, cols - 1));
+            }
+
+            for (int c = 0; c < cols - 1; c++)
+            {
+                queue.Enqueue((rows - 1, c));
+                queueHash.Add((rows - 1, c));
+            }
+
+            queue = BFS(queue, heights, false);
+
+         
+            return res;
+        }
+
+        private Queue<(int r, int c)> BFS(Queue<(int r, int c)> queue, int[][] heights, bool IsPacific)
+        {
+            while (queue.Count > 0)
+            {
+                var cell = queue.Dequeue();
+                queueHash.Remove(cell);
+
+                if (IsPacific)
+                    MakePacific(cell.r, cell.c);
+                else
+                    MakeAtlantic(cell.r, cell.c);
+
+                //bottom of the cell
+                if (CanMark((cell.r + 1, cell.c), heights, IsPacific))
+                {
+                    queue.Enqueue((cell.r + 1, cell.c));
+                    queueHash.Add((cell.r + 1, cell.c));
+                }
+
+                //right of the cell
+                if (CanMark((cell.r, cell.c + 1), heights, IsPacific))
+                {
+                    queue.Enqueue((cell.r, cell.c + 1));
+                    queueHash.Add((cell.r, cell.c + 1));
+                }
+
+                //top of the cell
+                if (CanMark((cell.r - 1, cell.c), heights, IsPacific))
+                {
+                    queue.Enqueue((cell.r - 1, cell.c));
+                    queueHash.Add((cell.r - 1, cell.c));
+                }
+
+                //left of the cell
+                if (CanMark((cell.r, cell.c - 1), heights, IsPacific))
+                {
+                    queue.Enqueue((cell.r, cell.c - 1));
+                    queueHash.Add((cell.r, cell.c - 1));
+                }
+            }
+            return queue;
+        }
+        private bool CanMark((int r, int c) cell, int[][] heights, bool IsPacific)
+        {
+
+            if (!CheckCellValid(cell) || AlreadyMarked(cell, IsPacific) || queueHash.Contains(cell))
+                return false;
+
+            var topCell = (cell.r - 1, cell.c);
+            if (CheckCellValid(topCell) && AlreadyMarked(topCell, IsPacific) && FlowToOcean(topCell, cell, heights))
+                return true;
+
+            var bottomCell = (cell.r + 1, cell.c);
+            if (CheckCellValid(bottomCell) && AlreadyMarked(bottomCell, IsPacific) && FlowToOcean(bottomCell, cell, heights))
+                return true;
+
+            var leftCell = (cell.r, cell.c - 1);
+            if (CheckCellValid(leftCell) && AlreadyMarked(leftCell, IsPacific) && FlowToOcean(leftCell, cell, heights))
+                return true;
+
+            var rightCell = (cell.r, cell.c + 1);
+            if (CheckCellValid(rightCell) && AlreadyMarked(rightCell, IsPacific) && FlowToOcean(rightCell, cell, heights))
+                return true;
+
+            return false;
+        }
+
+        private bool FlowToOcean((int r, int c) from, (int r, int c) to, int[][] heights)
+        {
+            return heights[from.r][from.c] <= heights[to.r][to.c];
+        }
+
+        private bool AlreadyMarked((int r, int c) cell, bool IsPacific)
+        {
+            if (flows[cell.r, cell.c] == 3)
+                return true;
+
+            if (IsPacific && flows[cell.r, cell.c] == 1)
+                return true;
+
+            if (!IsPacific && flows[cell.r, cell.c] == 2)
+                return true;
+
+            return false;
+        }
+
+        private bool CheckCellValid((int r, int c) cell)
+        {
+            return (cell.r >= 0 && cell.r < rows && cell.c >= 0 && cell.c < cols);
+        }
+
+        private void MakePacific(int r, int c)
+        {
+            if (flows[r, c] == 3)
+                return;
+
+            if (flows[r, c] == 0 || flows[r, c] == 2)
+                flows[r, c]++;
+
+            if (flows[r, c] == 3)
+                res.Add((r, c));
+        }
+
+        private void MakeAtlantic(int r, int c)
+        {
+            if (flows[r, c] == 3)
+                return;
+
+            if (flows[r, c] == 0 || flows[r, c] == 1)
+                flows[r, c] += 2;
+
+            if (flows[r, c] == 3)
+                res.Add((r, c));
+        }
+
+
+    }
+}
